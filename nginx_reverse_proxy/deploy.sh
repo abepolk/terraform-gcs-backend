5011b109f2064dfa7daca92cbeb9eda5eabe6c33
Abraham Polk <abepolk@gmail.com>
2023-09-27 20:29:36 -0400

    Add port to deploy script

--- nginx_reverse_proxy/deploy.sh
+++ nginx_reverse_proxy/deploy.sh
@@ -1,1 +1,1 @@
-gcloud run deploy test-service --image "us-east1-docker.pkg.dev/$(gcloud config get-value project)/test-nginx-html-repo/test-image"
+gcloud run deploy test-service --image "us-east1-docker.pkg.dev/$(gcloud config get-value project)/test-nginx-html-repo/test-image" --port 80
